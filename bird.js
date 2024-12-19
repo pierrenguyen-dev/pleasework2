e474f2af5eb1f067b58546b02a509b596793d067
pierrenguyen-dev <pierre.ngn.belgium@gmail.com>
2024-12-19 22:02:51 +0100

    s

--- bird.js
+++ bird.js
@@ -1,4 +1,4 @@
-import { BIRDSIZE, CANVAS_HEIGHT, CANVAS_WIDTH, BIRDANIMATIONFRAME, FLOOROFFSET } from './constants';
+import { BIRDSIZE, CANVAS_HEIGHT, CANVAS_WIDTH, BIRDANIMATIONFRAME, FLOOROFFSET } from './constants.js';
 
 
 export default class Bird {
